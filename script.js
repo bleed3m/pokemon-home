309f0174f10f5f8b1eb75b63217a07d47bb7d608
Bleedem <jjacobwow@gmail.com>
2024-01-29 23:23:54 -0500

    item + link update

--- script.js
+++ script.js
@@ -10,23 +10,38 @@ const database = getDatabase(app)
 const shoppingListInDB = ref(database, "shoppingList")
 
 const inputFieldEl = document.getElementById("input-field")
+const linkFieldEl = document.getElementById("link-field")
 const addButtonEl = document.getElementById("add-button")
+const clearButtonEl = document.getElementById("clear-button")
 const shoppingListEl = document.getElementById("shopping-list")
+
+inputFieldEl.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault()
+        document.getElementById("add-button").click()
+    }
+})
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
+    let linkValue = linkFieldEl.value
     
     push(shoppingListInDB, inputValue)
     
-    clearInputFieldEl()
+    clearFieldsEl()
     
-    appendToShoppingList(inputValue)
+    appendToShoppingList(inputValue, linkValue)
 })
 
-function clearInputFieldEl() {
+clearButtonEl.addEventListener("click", function() {
+    shoppingListEl.innerHTML = ""
+})
+
+function clearFieldsEl() {
     inputFieldEl.value = ""
+    linkFieldEl.value = ""
 }
 
-function appendToShoppingList(itemValue) {
-    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+function appendToShoppingList(itemValue, linkValue) {
+    shoppingListEl.innerHTML += `<a href='${linkValue}' target='_blank'><li>${itemValue}</li></a>`
 }
